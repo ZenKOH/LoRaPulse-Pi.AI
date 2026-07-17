7f1a7c9d425af8cffe94d2d2771d6369ab5054d7
Zen KOH <36438318+ZenKOH@users.noreply.github.com>
2026-07-17 21:49:43 +0800

    Add Pico sensor stub

---
+++ firmware/pico_sx1262_node/sensors.py
@@ -0,0 +1,10 @@
+# MicroPython sensor stub for Pico-class LoRa nodes.
+
+
+def read_operational_sample():
+    return {
+        "temperature": [24.1, 24.2, 24.3],
+        "battery": 83,
+        "faults": [],
+        "session_events": [],
+    }
